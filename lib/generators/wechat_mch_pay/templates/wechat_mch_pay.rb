183c7716fe64307bd65029335d079a03cd67b3be
rqiang <qiang.ruan@mdslife.com>
2019-11-06 09:51:31 +0800

    pay

--- lib/generators/wechat_mch_pay/templates/wechat_mch_pay.rb
+++ lib/generators/wechat_mch_pay/templates/wechat_mch_pay.rb
@@ -3,6 +3,8 @@ WechatMchPay.configuration do |config|
   config.mch_id    = '商户号'    # null: false
   config.app_key   = 'app_key'  # null: false
   config.send_name = '商户名称'  # null: false
+  config.ssl_client_key = '证书key地址'    # null: false
+  config.ssl_client_cert = '证书秘钥地址'   # null: false
   config.client_ip = 'client_ip'
-  config.notify_url = 'notify_url'
+  config.notify_url = '微信通知地址'
 end
